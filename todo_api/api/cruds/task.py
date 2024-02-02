3e9c10db5a492612ab82375bb348f26ecf433e02
Yuki Fujita <uni22yu@gmail.com>
2024-02-02 19:39:42 +0900

    add/update taks

--- todo_api/api/cruds/task.py
+++ todo_api/api/cruds/task.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 from sqlalchemy import select
 from sqlalchemy.engine import Result
@@ -29,3 +29,21 @@ async def create_task(
     await db.commit()
     await db.refresh(task)
     return task
+
+
+async def get_task(db: AsyncSession, task_id: int) -> Optional[task_model.Task]:
+    result: Result = await db.execute(
+        select(task_model.Task).filter(task_model.Task.id == task_id)
+    )
+    task: Optional[Tuple[task_model.Task]] = result.first()
+    return task[0] if task is not None else None  # 要素が一つであってもtupleで返却されるので１つ目の要素を取り出す
+
+
+async def update_task(
+    db: AsyncSession, task_create: task_schema.TaskCreate, original: task_model.Task
+) -> task_model.Task:
+    original.title = task_create.title
+    db.add(original)
+    await db.commit()
+    await db.refresh(original)
+    return original
